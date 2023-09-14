7bbe9e1d33cc31a0c664b8fc7c3206a0900c66f3
Ankit Prajapati <code7lancer@gmail.com>
2023-09-14 11:09:02 -0700

    created auth schema

---
+++ schemas/auth.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose")
+
+const authSchema = mongoose.Schema({
+    name: String,
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+module.exports = mongoose.model("auth", authSchema)
